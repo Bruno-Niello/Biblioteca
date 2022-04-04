309d20b81ff8ddfad6e84b44b2d044db66fed2f4
Bruno Niello <brunoniello@gmail.com>
2022-04-04 20:15:48 -0300

    class libro

--- Script/main.js
+++ Script/main.js
@@ -1,19 +1,33 @@
 //ingreso de usuario 
 
-// let pass = 1234567890;
-// let dato = parseInt(prompt("cual es tu contraseña?"));
-// let intentos = 3;
+let pass = 1234567890;
+let dato = parseInt(prompt("cual es tu contraseña?"));
+let intentos = 3;
 
-// while (pass != dato) {
-//     if (intentos > 0) {
-//         intentos--; 
-//         alert(`contraseña es erronea. te quedan ${intentos} intentos`);
-//         dato = parseInt(prompt("cual es tu contraseña?"));
-//     } else {
-//         alert("no quedan intentos");
-//         break;
-//     }
-// }
+while (pass != dato) {
+    if (intentos > 0) {
+        intentos--; 
+        alert(`contraseña es erronea. te quedan ${intentos} intentos`);
+        dato = parseInt(prompt("cual es tu contraseña?"));
+    } else {
+        alert("no quedan intentos");
+        break;
+    }
+}
+
+class Libro {
+    constructor(titulo, autor, editorial) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial; 
+        stock = true;
+    }
+    prestamo() {
+        if (stock == true){
+            stock = false;
+            return stock;}
+    }
+}
 
 class Usuario {
     constructor(nombre, direccion, telefono, mail){
